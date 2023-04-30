affb7dc8d13fa90fa586b3a29d53c1bf6c3c6a42
Roman Gorobets <Gorobetcrv@gmail.com>
2023-05-01 02:34:27 +0300

    HW_07

---
+++ js/01-gallery.js
@@ -0,0 +1,62 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+{/* <li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</li> */}
+
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onImgClick);
+
+// rendered
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+
+const instance = basicLightbox.create(
+  `
+<img width="1280" height="auto" src="">`,
+  {
+    onShow: (instance) => {
+      window.addEventListener('keydown', logKey);
+    },
+    onClose: (instance) => {
+      window.removeEventListener('keydown', logKey);
+    },
+  }
+);
+
+function onImgClick(e) {
+  e.preventDefault();
+  const datasetSource = e.target.dataset.source;
+  if (!datasetSource) return;
+  instance.element().querySelector('img').src = datasetSource;
+  instance.show();
+}
+
+function logKey(e) {
+  if (e.code !== 'Escape') return;
+  instance.close();
+}
